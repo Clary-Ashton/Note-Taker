3aa06bca11f60dfadc4dce20ee6e98be8c7d5718
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-19 22:38:58 +0000

    server.js running

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
+const allNotes = require('./db/db.json')
 
 // Setting up the server
 const app = express();
@@ -13,6 +14,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+
+app.listen(PORT, () => 
+    console.log(`App listening now at localhost://${PORT}`)
+);
+
 
 
 
