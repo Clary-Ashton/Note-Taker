dba6fdda17a5ed1566043b3735c8e0190c026d82
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-19 02:25:39 +0000

    Setting up Middleware

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+
+
