dc043afa604da1c442a8bb7efb6b23df76e73821
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-20 00:12:01 +0000

    Added html routes for Get/notes and Get *

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const allNotes = require('./db/db.json')
+const apiRoutes = require('./routes/notes');
 
 // Setting up the server
 const app = express();
@@ -14,6 +14,22 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use('./api/notes', apiRoutes)
+
+//Get/notes return the notes.html file.
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(_dirname, 'public/notes.html'))
+);
+
+//Get * return return the index.html file.
+app.get('/*', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+
+
+
 
 
 app.listen(PORT, () => 
