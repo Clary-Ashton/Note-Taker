8b97998fecff725a58d38c03b281146fdf4b1f56
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-20 01:45:49 +0000

    Added routes/notes to seperate files

---
+++ routes/notes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const fs = require('fs');
+const router = express.Router();
+const util = require('util');
+const uuid = require('../helpers/uuid');
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
