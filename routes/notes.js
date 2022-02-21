a436093457605903d55cea0492fa9569a96b31db
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-21 01:02:45 +0000

    router updated (content, file)

--- routes/notes.js
+++ routes/notes.js
@@ -46,16 +46,27 @@ readFromFile('./db/db.json')
 })
 .catch((err) => console.log("Error:", err));
 
-
 })
 
 
+const readFromFile = util.promisify(fs.readFile);
 
-
-
-
-
-
+const writeToFile = (destination, content) =>
+ fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+   err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
+ 
+const readAndAppend = (content, file) => {
+ fs.readFile(file, 'utf8', (err, data) => {
+   if (err) {
+     console.error(err);
+   } else {
+     const parsedData = JSON.parse(data);
+     parsedData.push(content);
+     writeToFile(file, parsedData);
+   }
+ });
+};
 
 
 
