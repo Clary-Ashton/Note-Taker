9326159aef285fe6aa209c32ff177124d83ebccc
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-02-21 00:48:48 +0000

    Adding router.get, router.post and router.delete

--- routes/notes.js
+++ routes/notes.js
@@ -4,6 +4,51 @@ const router = express.Router();
 const util = require('util');
 const uuid = require('../helpers/uuid');
 
+//Get route for retrieving all notes
+router.get("/", (req, res) =>{
+console.info(`${req.method} request received.`);
+readFromFile(`./db/db.json`)
+.then((data) => res.json(JSON.parse(data)))
+.catch((err) => console.log("Error:", err));
+});
+
+
+//Post route for submitting a new note
+router.post('/', (req, res) =>{
+    console.info(`${req.method} request received to add a new note.`);
+
+    const { title, text } = req.body;
+
+    if (req.body){
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully!`);
+    } else {
+        res.error('Error in adding note!');
+    }
+
+})
+
+//Delete route for deletting a note
+router.delete('/:id', (req, res) =>{
+console.info(`${req.method} request received.`);
+readFromFile('./db/db.json')
+.then((data) =>{
+    const notes = JSON.parse(data);
+    res.json(filteredNotes);
+    writeToFile('./db/db.json', filteredNotes);
+})
+.catch((err) => console.log("Error:", err));
+
+
+})
+
 
 
 
